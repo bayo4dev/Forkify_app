6f986e0649108ff44c62363a81b03eb749511a5f
Adebayo Emmanuel <adebayoayodele677@gmail.com>
2024-09-19 12:38:53 +0100

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "96df00c3-3a78-429b-b4cc-1331da98eb06";
+export const MODAL_CLOSE_SEC = 2.5;
